52509972b64f8f1f653bbe96bb2c38c67cae30f8
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-03-15 00:37:13 +0100

    Add CLI options constants

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/cli/CLIOptions.java
@@ -0,0 +1,16 @@
+package at.jku.ssw.java.bytecode.reducer.cli;
+
+/**
+ * Holds constants related to the CLI parameters.
+ */
+public interface CLIOptions {
+    String HELP       = "help";
+    String VERSION    = "help";
+    String QUIET      = "q";
+    String VERBOSE    = "v";
+    String SEQUENTIAL = "s";
+    String N_THREADS  = "n";
+    String OUT        = "out";
+    String TEMP       = "tmp";
+    String I_TESTS    = "i";
+}
