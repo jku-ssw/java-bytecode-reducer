a5f30291094c35d964754e754eb2e1a7ae65591d
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-03-17 03:35:32 +0100

    Add helper for the current test directory

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/io/TestDirectory.java
@@ -0,0 +1,154 @@
+package at.jku.ssw.java.bytecode.reducer.io;
+
+import at.jku.ssw.java.bytecode.reducer.context.Context;
+import at.jku.ssw.java.bytecode.reducer.utils.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * This class takes care of the file handling for a single test instance.
+ * This includes the initiation of the test files - copying the class files,
+ * interestingness tests - as well as the clean up afterwards - purging the
+ * test directory or copying the new intermediate solution to the
+ * output directory.
+ * The structure of the test directory itself is as follows
+ * (all test directories are contained in the
+ * {@link at.jku.ssw.java.bytecode.reducer.context.ContextFactory#DEFAULT_TEMP} folder):
+ * <ul>
+ * <li>&#123;test-dir-name&#125;
+ * <ul>
+ * <li>&#123;class-file-1&#125;</li>
+ * <li>&#123;class-file-2&#125;</li>
+ * <li>&#123;...&#125;</li>
+ * <li>&#123;class-file-n&#125;</li>
+ * <li>&#123;interestingness-test-1&#125;</li>
+ * <li>&#123;interestingness-test-2&#125;</li>
+ * <li>&#123;...&#125;</li>
+ * <li>&#123;interestingness-test-n&#125;</li>
+ * </ul>
+ * </li>
+ * </ul>
+ */
+public class TestDirectory {
+    //-------------------------------------------------------------------------
+    // region Constants
+
+    private static final Logger logger = LogManager.getLogger();
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region Properties
+
+    /**
+     * The name of the current directory.
+     * This name must be unique in order to prevent the tasks to override other
+     * existing test files.
+     * After the test directory is purged, this name can be reassigned.
+     */
+    public final String name;
+
+    /**
+     * The current test context. Holds properties like the different paths.
+     */
+    private final Context context;
+
+    /**
+     * This directory's path.
+     */
+    private Path path;
+
+    /**
+     * Flag that indicates that the directory has already been cleared.
+     */
+    private boolean purged;
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region Constructors
+
+    public TestDirectory(Context context, String name) throws IOException {
+        assert context != null : "Cannot initialize test directory without context";
+        assert StringUtils.isNotBlank(name) : "Cannot initialize nameless test directory";
+
+        this.name = name;
+        this.context = context;
+
+        this.purged = false;
+
+        // Set the current test directory
+        this.path = Files.createDirectories(context.tempDir.resolve(name));
+
+        // Copy the source files
+        copy(context.classFiles, this.path);
+        copy(context.iTests, this.path);
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region Directory management methods
+
+    public boolean lift() throws IOException {
+        if (purged) return false;
+
+        final Path out = context.outDir;
+
+        // Copy the files into the output directory
+        Files.list(path)
+                .forEach(p -> {
+                    try {
+                        Files.copy(p, out.resolve(p.getFileName()));
+                    } catch (IOException e) {
+                        logger.fatal("ERROR: Could not copy result file {} to output directory {}. The current results lie in {}: {}", p, out, path, e.getMessage());
+                    }
+                });
+
+        return true;
+    }
+
+    public boolean clear() {
+        if (purged) return true;
+
+        try {
+            return purged = Files.walk(path)
+                    .map(Path::toFile)
+                    .allMatch(File::delete);
+        } catch (IOException e) {
+            logger.fatal("ERROR: Could not clear test directory {}: {}", path, e.getMessage());
+        }
+
+        return false;
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region IO utilities
+
+    private void delete(List<Path> src) {
+        src.forEach(p -> {
+            try {
+                Files.delete(p);
+            } catch (IOException e) {
+                logger.fatal("ERROR: Could not delete file {} from test directory: {}", p, e.getMessage());
+            }
+        });
+    }
+
+    private void copy(List<Path> src, Path dst) {
+        src.forEach(p -> {
+            try {
+                Files.copy(p, dst.resolve(p.getFileName()));
+            } catch (IOException e) {
+                logger.fatal("ERROR: Could not copy file {} to test directory {}: {}", p, dst, e.getMessage());
+            }
+        });
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
+}
