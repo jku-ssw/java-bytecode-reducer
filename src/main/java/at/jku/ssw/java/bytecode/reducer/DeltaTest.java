b914c9532a7acd09443b558b19ab4da6b7631b67
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-03-18 23:29:18 +0100

    [WIP] Add class that handles reduction + validation for a reducer

--- src/main/java/at/jku/ssw/java/bytecode/reducer/DeltaTest.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/DeltaTest.java
@@ -1,28 +1,72 @@
 package at.jku.ssw.java.bytecode.reducer;
 
+import at.jku.ssw.java.bytecode.reducer.context.Context;
+import at.jku.ssw.java.bytecode.reducer.io.ScriptRunner;
+import at.jku.ssw.java.bytecode.reducer.io.TestDirectory;
 import at.jku.ssw.java.bytecode.reducer.reducers.Reducer;
+import javassist.CtClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
-// TODO make runnable?
-public class DeltaTest {
-    private final String  name;
-    private final Reducer reducer;
+public class DeltaTest implements Runnable {
+    private static final Logger logger = LogManager.getLogger();
 
-    public DeltaTest(String name, Reducer reducer) {
-        this.name = name;
+    private final Reducer       reducer;
+    private final Context       context;
+    private final ScriptRunner  runner;
+    private final TestDirectory dir;
+
+    public DeltaTest(String name, Reducer reducer, Context context) throws IOException {
         this.reducer = reducer;
+        this.context = context;
+        // create a new test directory
+        this.dir = new TestDirectory(context, name + "_" + reducer);
+        runner = new ScriptRunner();
     }
 
-    public void initTestDir(Path from) throws IOException {
-//        Path workingDir = Paths.get(".");
-//        Path outDir     = workingDir.resolve(JReduce.TEMP_DIR).resolve(name);
+    @Override
+    public void run() {
+        logger.info("{}: Initializing test using Reducer {}", this, reducer);
+//        // TODO
+//        context.classFiles.stream()
+//                .map(this::loadClass)
+//                .peek(reducer::transform)
+//                .map(this::writeClass);
 //
-//        FileUtils.copyDirectory(from.toFile(), outDir.toFile());
+//        final CtClass clazz = loadClass();
+//        reducer.transform(clazz);
+        logger.info("{}: Finished reduction {}", this, reducer);
+
+        try {
+            if (!finish()) {
+                logger.fatal("{}: Reduction finished but file access failed");
+            }
+        } catch (IOException e) {
+            logger.fatal("ERROR: Could not finish test {}: {}", this, e.getMessage());
+        }
+
+        logger.info("{}: Cleared test directory", this);
     }
 
-    public void transform() {
-//        initTestDir();
+    @Override
+    public String toString() {
+        return dir.toString();
+    }
+
+    private CtClass loadClass(Path path) {
+
+        return null;
+    }
+
+    private boolean writeClass(CtClass clazz) {
+
+        return false;
+    }
+
+    private synchronized boolean finish() throws IOException {
+        return dir.lift() && dir.clear();
     }
 }
