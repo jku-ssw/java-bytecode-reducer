63befb2e58a974b4dd0fad6a7f0a48227870e531
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-03-20 22:39:05 +0100

    Add base reducer for unused fields (dummy implementation)

--- src/main/java/at/jku/ssw/java/bytecode/reducer/reducers/RemoveUnusedFields.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/reducers/RemoveUnusedFields.java
@@ -1,10 +1,20 @@
 package at.jku.ssw.java.bytecode.reducer.reducers;
 
 import javassist.CtClass;
+import javassist.NotFoundException;
+
+import java.util.Arrays;
 
 public class RemoveUnusedFields implements Reducer {
     public CtClass transform(CtClass clazz) {
-//        return clazz.getDeclaredFields()
+        Arrays.stream(clazz.getDeclaredFields())
+                .forEach(f -> {
+                    try {
+                        clazz.removeField(f);
+                    } catch (NotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
         return clazz;
     }
 }
