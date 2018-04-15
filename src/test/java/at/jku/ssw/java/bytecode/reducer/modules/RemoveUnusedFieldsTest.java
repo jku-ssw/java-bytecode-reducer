8ff604dae1cd94817b52ba3cee8539a9fda4db15
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-04-15 03:24:23 +0200

    Add scaffolding for RemoveUnusedFieldsTest

---
+++ src/test/java/at/jku/ssw/java/bytecode/reducer/modules/RemoveUnusedFieldsTest.java
@@ -0,0 +1,29 @@
+package at.jku.ssw.java.bytecode.reducer.modules;
+
+import at.jku.ssw.java.bytecode.reducer.ModuleTest;
+
+public class RemoveUnusedFieldsTest extends ModuleTest<RemoveUnusedFields> {
+    void testNoFields() {
+
+    }
+
+    void testOnlyUsedFields() {
+
+    }
+
+    void testOnlyUnusedFields() {
+
+    }
+
+    void testStaticFields() {
+
+    }
+
+    void testPrivateFields() {
+
+    }
+
+    void testPublicFields() {
+
+    }
+}
