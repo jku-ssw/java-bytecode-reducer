9dbcbb49e7ca3c00657dc7e007b525e22dee880d
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-04-15 03:23:03 +0200

    Add string utils

--- src/main/java/at/jku/ssw/java/bytecode/reducer/utils/StringUtils.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/utils/StringUtils.java
@@ -4,6 +4,13 @@ package at.jku.ssw.java.bytecode.reducer.utils;
  * Utility methods to extends the functionality of {@link java.lang.String}s.
  */
 public final class StringUtils {
+
+    /**
+     * Regular expression to identify camel case strings and split
+     * them accordingly.
+     */
+    private static final String CAMEL_CASE_REGEX = "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])";
+
     private StringUtils() {
     }
 
@@ -27,5 +34,15 @@ public final class StringUtils {
      */
     public static boolean isNotBlank(String str) {
         return !isBlank(str);
+    }
+
+    /**
+     * Converts the given camel-cased string to snake-case.
+     *
+     * @param str The camel-cased string
+     * @return a snake-cased string
+     */
+    public static String snake_case(String str) {
+        return String.join("_", str.split(CAMEL_CASE_REGEX));
     }
 }
