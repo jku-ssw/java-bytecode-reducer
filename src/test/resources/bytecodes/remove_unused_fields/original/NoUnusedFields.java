d1a30ce4c40ed132f0e7ba5581946ba212fc6278
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-04-15 22:02:28 +0200

    [RemoveUnusedFields] Add resource byte codes

---
+++ src/test/resources/bytecodes/remove_unused_fields/original/NoUnusedFields.java
@@ -0,0 +1,9 @@
+public class NoUnusedFields {
+    public int anInt;
+
+    void aMethod() {
+        int i = anInt;
+    }
+
+    public static void main(String[] args) {}
+}
---
+++ src/test/resources/bytecodes/remove_unused_fields/original/UnusedFields.java
@@ -0,0 +1,9 @@
+public class UnusedFields {
+    public int anInt;
+    public static boolean aBoolean;
+    private static float aFloat;
+
+    public static void main(String[] args) {
+        System.out.println("42");
+    }
+}
