73b562495e082c62ce11ab963135c33b29536ee4
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-05-07 13:21:09 +0200

    Refactor `FileUtils`

--- src/main/java/at/jku/ssw/java/bytecode/reducer/utils/FileUtils.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/utils/FileUtils.java
@@ -1,15 +1,22 @@
 package at.jku.ssw.java.bytecode.reducer.utils;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public final class FileUtils {
-    private FileUtils() {}
+    private FileUtils() {
+    }
 
+    /**
+     * Helper that recursively iterates a directory and deletes
+     * its file contents before clearing the directory itself.
+     *
+     * @param path The path to the directory
+     * @return the deleted directory path
+     * @throws IOException if the path does not denote a directory
+     *                     or IO errors occur
+     */
     public static Path delete(Path path) throws IOException {
         if (path == null)
             throw new IllegalArgumentException("Path must not be null");
@@ -27,5 +34,21 @@ public final class FileUtils {
                 return FileVisitResult.CONTINUE;
             }
         });
+    }
+
+    /**
+     * Helper method that checks whether the given path - assumed to be a
+     * directory - is empty.
+     * Uses a stream check instead of a content list that may perform better
+     * in large non-empty directories.
+     *
+     * @param path The path (denoting a director)
+     * @return true, if the directory is empty; false if it is not
+     * @throws IOException if the path is not a directory or IO access fails
+     */
+    public static boolean isEmpty(Path path) throws IOException {
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(path)) {
+            return !dirStream.iterator().hasNext();
+        }
     }
 }
