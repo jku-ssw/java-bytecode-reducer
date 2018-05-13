38c7ea8b006cf251c356097949f3a8e7805c2057
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-05-14 01:47:34 +0200

    Add helper to get default values for types

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/utils/Types.java
@@ -0,0 +1,32 @@
+package at.jku.ssw.java.bytecode.reducer.utils;
+
+import java.util.Map;
+
+public class Types {
+    private static final Map<Class<?>, Object> defaults = Map.of(
+            byte.class, (byte) 0,
+            short.class, (short) 0,
+            int.class, 0,
+            long.class, 0L,
+            float.class, 0.0F,
+            double.class, 0.0,
+            char.class, '\0',
+            boolean.class, Boolean.FALSE
+    );
+
+    // TODO check wrapper class default handling
+
+    /**
+     * Returns the default value that is automatically a assigned to fields
+     * of the given type.
+     *
+     * @param type The type for the required default value
+     * @param <T>  The generic type of the value
+     * @return the default value for the given primitive type
+     * or null for reference types
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T defaults(Class<T> type) {
+        return (T) defaults.getOrDefault(type, null);
+    }
+}
