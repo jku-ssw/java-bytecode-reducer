d3d891d34df48feee2674a4f78087199a63f5626
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-05-18 14:14:41 +0200

    [RemoveEmptyMethods] Finish implementation (UNTESTED)

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/methods/RemoveEmptyMethods.java
@@ -0,0 +1,41 @@
+package at.jku.ssw.java.bytecode.reducer.modules.methods;
+
+import at.jku.ssw.java.bytecode.reducer.annot.Sound;
+import at.jku.ssw.java.bytecode.reducer.runtypes.MemberReducer;
+import at.jku.ssw.java.bytecode.reducer.utils.Javassist;
+import javassist.CtClass;
+import javassist.CtMethod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@Sound
+public class RemoveEmptyMethods implements MemberReducer<CtClass, CtMethod> {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public CtClass classFrom(byte[] bytecode) throws Exception {
+        return Javassist.loadClass(bytecode);
+    }
+
+    @Override
+    public byte[] bytecodeFrom(CtClass clazz) throws Exception {
+        return Javassist.bytecode(clazz);
+    }
+
+    @Override
+    public Stream<CtMethod> getMembers(CtClass clazz) throws Exception {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(CtMethod::isEmpty);
+    }
+
+    @Override
+    public CtClass process(CtClass clazz, CtMethod m) throws Exception {
+        logger.debug("Removing unused method '{}'", m.getSignature());
+        clazz.removeMethod(m);
+        return clazz;
+    }
+}
