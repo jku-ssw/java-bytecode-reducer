19aeb985644b69ac1589bd4cf1efd59fd9a3b620
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-02 00:14:53 +0200

    Update bytecodes

--- src/test/resources/bytecodes/remove_unused_fields/original/UnusedFields.java
+++
@@ -1,9 +0,0 @@
-public class UnusedFields {
-    public int anInt;
-    public static boolean aBoolean;
-    private static float aFloat;
-
-    public static void main(String[] args) {
-        System.out.println("42");
-    }
-}
---
+++ src/test/resources/bytecodes/remove_unused_fields/reduced/NoUsedFields.java
@@ -0,0 +1,4 @@
+public class NoUsedFields {
+    public static void main(String[] args) {
+    }
+}
