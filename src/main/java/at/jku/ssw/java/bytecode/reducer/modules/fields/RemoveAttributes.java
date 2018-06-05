d2f8d9e6ceccd844f2ba370f9889fbcf9f8cd1f4
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-05 16:40:21 +0200

    [RemoveAttributes] Add implementation

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/fields/RemoveAttributes.java
@@ -0,0 +1,42 @@
+package at.jku.ssw.java.bytecode.reducer.modules.fields;
+
+import at.jku.ssw.java.bytecode.reducer.runtypes.MemberReducer;
+import javassist.CtClass;
+import javassist.CtField;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static at.jku.ssw.java.bytecode.reducer.utils.Javassist.bytecode;
+import static at.jku.ssw.java.bytecode.reducer.utils.Javassist.loadClass;
+
+public class RemoveAttributes implements MemberReducer<CtClass, CtField> {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public CtClass classFrom(byte[] bytecode) throws Exception {
+        return loadClass(bytecode);
+    }
+
+    @Override
+    public byte[] bytecodeFrom(CtClass clazz) throws Exception {
+        return bytecode(clazz);
+    }
+
+    @Override
+    public Stream<CtField> getMembers(CtClass clazz) {
+        return Arrays.stream(clazz.getDeclaredFields());
+    }
+
+    @Override
+    public CtClass process(CtClass clazz, CtField field) {
+        logger.debug("Removing attributes of field '{}'", field.getSignature());
+
+        field.setModifiers(0);
+
+        return clazz;
+    }
+}
