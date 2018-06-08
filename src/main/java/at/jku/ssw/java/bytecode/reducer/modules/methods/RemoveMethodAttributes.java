76249d02d356b4fad3bde63b5ae29558a7c2b44a
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-08 16:31:04 +0200

    [RemoveMethodAttributes] Add implementation

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/methods/RemoveMethodAttributes.java
@@ -0,0 +1,48 @@
+package at.jku.ssw.java.bytecode.reducer.modules.methods;
+
+import at.jku.ssw.java.bytecode.reducer.annot.Unsound;
+import at.jku.ssw.java.bytecode.reducer.runtypes.MemberReducer;
+import at.jku.ssw.java.bytecode.reducer.utils.Javassist;
+import javassist.CtClass;
+import javassist.CtMethod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static at.jku.ssw.java.bytecode.reducer.utils.Javassist.bytecode;
+import static at.jku.ssw.java.bytecode.reducer.utils.Javassist.loadClass;
+
+@Unsound
+public class RemoveMethodAttributes implements MemberReducer<CtClass, CtMethod> {
+
+    public static final int NO_ATTRIBUTES = 0x0;
+
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public CtClass classFrom(byte[] bytecode) throws Exception {
+        return loadClass(bytecode);
+    }
+
+    @Override
+    public byte[] bytecodeFrom(CtClass clazz) throws Exception {
+        return bytecode(clazz);
+    }
+
+    @Override
+    public Stream<CtMethod> getMembers(CtClass clazz) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> !Javassist.isMain(m));
+    }
+
+    @Override
+    public CtClass process(CtClass clazz, CtMethod method) {
+        logger.debug("Removing attributes of method '{}'", method.getLongName());
+
+        method.setModifiers(NO_ATTRIBUTES);
+
+        return clazz;
+    }
+}
