fb7c4c81cc354a3590f31b2ccd0b18e911420f50
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-14 16:57:05 +0200

    [RemoveUnusedFields] Remove commented out code

--- src/main/java/at/jku/ssw/java/bytecode/reducer/modules/fields/RemoveUnusedFields.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/fields/RemoveUnusedFields.java
@@ -50,43 +50,9 @@ public class RemoveUnusedFields implements MemberReducer<CtClass, CtField> {
                 (TConsumer<FieldAccess>) f -> f.replace("")
         );
 
-//        removeAccesses(clazz, field);
-
         clazz.removeField(field);
 
         return clazz;
     }
 
-//    private void removeAccesses(CtClass clazz, CtField field) {
-//        Arrays.stream(clazz.getDeclaredBehaviors()).forEach((TConsumer<CtBehavior>) m -> {
-//            MethodInfo methodInfo = m.getMethodInfo();
-//            ConstPool  constPool  = methodInfo.getConstPool();
-//
-//            CodeIterator it = methodInfo.getCodeAttribute().iterator();
-//
-//            while (it.hasNext()) {
-//                int index = it.next();
-//                int op    = it.byteAt(index);
-//
-//                // check object access
-////                if (op == Opcode.ALOAD || op >= Opcode.ALOAD_0 && op <= Opcode.ALOAD_3) {
-////                    for (int nextIndex = it.next(),
-////                         nextOp = it.byteAt(nextIndex);
-////                            it.hasNext() && nextOp != Opcode.ASTORE && (nextOp < Opcode.ASTORE_0 || nextOp > Opcode.ASTORE_3);
-////                            nextIndex = it.next(),
-////                            nextOp = it.byteAt(nextIndex)){
-//                if (op == Opcode.PUTFIELD) {
-//                    int    fieldIndex = it.u16bitAt(index + 1);
-//                    String name       = constPool.getFieldrefName(fieldIndex);
-//
-//                    if (field.getName().equals(name)) {
-//                        System.out.println("PUTFIELD " + name);
-//                        it.writeByte(Opcode.NOP, index);
-//                        it.writeByte(Opcode.NOP, index + 1);
-//                        it.writeByte(Opcode.NOP, index + 2);
-//                    }
-//                }
-//            }
-//        });
-//    }
 }
