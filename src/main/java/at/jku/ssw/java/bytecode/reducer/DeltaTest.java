f5c50313818cddc5b925518e398d9bdcb24ad439
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-18 17:02:15 +0200

    Ignore legacy test setup files for now

--- src/main/java/at/jku/ssw/java/bytecode/reducer/DeltaTest.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/DeltaTest.java
@@ -1,146 +1,125 @@
 package at.jku.ssw.java.bytecode.reducer;
 
-import at.jku.ssw.java.bytecode.reducer.context.Context;
-import at.jku.ssw.java.bytecode.reducer.io.ScriptRunner;
-import at.jku.ssw.java.bytecode.reducer.io.TestDirectory;
-import at.jku.ssw.java.bytecode.reducer.runtypes.Reducer;
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.NotFoundException;
-import javassist.bytecode.ClassFilePrinter;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.io.IoBuilder;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-public class DeltaTest implements Runnable {
-    private static final Logger logger = LogManager.getLogger();
-
-    private final String       name;
-    private final Reducer      reducer;
-    private final Context      context;
-    private final ScriptRunner runner;
-
-    /**
-     * The current directory
-     */
-    private final TestDirectory dir;
-    private final ClassPool     cp;
-
-    public DeltaTest(String name, Reducer reducer, Context context) throws IOException {
-        this.name = name + "_" + reducer.getClass().getSimpleName();
-        this.reducer = reducer;
-        this.context = context;
-        // create a new test directory
-        this.dir = new TestDirectory(context, this.name);
-        runner = new ScriptRunner();
-        cp = ClassPool.getDefault();
-    }
-
-    @Override
-    public void run() {
-        debug("Initializing reducer");
-
-        // load the individual classes and transform them
-        // TODO maybe separate / run tests per reduction on file
-        try {
-            for (Path classFile : context.classFiles) {
-                reduce(classFile);
-            }
-        } catch (Exception e) {
-            fatal("Could not complete reduction: {}", e.getMessage());
-            e.printStackTrace();
-        }
-
-        debug("Finished reduction", this, reducer);
-
-        // TODO use return values
-        if (test()) {
-            info("Test successful");
-            try {
-                if (dir.lift()) {
-                    dir.clear();
-                } else {
-                    fatal("Could not copy result files to output directory");
-                }
-            } catch (IOException e) {
-                fatal("Could not copy result files to output directory: {}", e.getMessage());
-            }
-        } else {
-            info("Test failed");
-            dir.clear();
-        }
-
-        debug("Test directory cleared");
-        debug("{}: Cleared test directory", this);
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    private CtClass loadClass(Path path) throws IOException {
-        try (InputStream is = Files.newInputStream(path)) {
-            CtClass clazz = cp.makeClass(is);
-            print(clazz);
-            return clazz;
-        }
-    }
-
-    private void writeClass(CtClass clazz) throws IOException, CannotCompileException, NotFoundException {
-        print(clazz);
-        clazz.writeFile();
-    }
-
-    private void reduce(Path path) throws Exception {
-        CtClass clazz = loadClass(path);
-//        clazz = reducer.apply();
-        writeClass(clazz);
-    }
-
-    private boolean test() {
-        return context.iTests.stream().map(i -> {
-            try {
-                return runner.exec(i);
-            } catch (IOException e) {
-                fatal("Could not complete test, as the script reported errors: {}", e.getMessage());
-                return null;
-            }
-        }).allMatch(p -> {
-            try {
-                return p != null && p.waitFor() == ScriptRunner.EXIT_SUCCESS;
-            } catch (InterruptedException e) {
-                fatal("Could not complete test, as the process was interrupted: {}", e.getMessage());
-                return false;
-            }
-        });
-
-    }
-
-    private void print(CtClass clazz) {
-        PrintWriter writer = IoBuilder.forLogger()
-                .setLevel(Level.DEBUG)
-                .buildPrintWriter();
-        ClassFilePrinter.print(clazz.getClassFile(), writer);
-    }
-
-    private void debug(String message, Object... params) {
-        logger.debug(this + ": " + message, params);
-    }
-
-    private void info(String message, Object... params) {
-        logger.info(this + ": " + message, params);
-    }
-
-    private void fatal(String message, Object... params) {
-        logger.fatal(this + ": " + message, params);
-    }
+public class DeltaTest {
+//    private static final Logger logger = LogManager.getLogger();
+//
+//    private final String       name;
+//    private final Reducer      reducer;
+//    private final Context      context;
+//    private final ScriptRunner runner;
+//
+//    /**
+//     * The current directory
+//     */
+//    private final TestDirectory dir;
+//    private final ClassPool     cp;
+//
+//    public DeltaTest(String name, Reducer reducer, Context context) throws IOException {
+//        this.name = name + "_" + reducer.getClass().getSimpleName();
+//        this.reducer = reducer;
+//        this.context = context;
+//        // create a new test directory
+//        this.dir = new TestDirectory(context, this.name);
+//        runner = new ScriptRunner();
+//        cp = ClassPool.getDefault();
+//    }
+//
+//    public void run() {
+//        debug("Initializing reducer");
+//
+//        // load the individual classes and transform them
+//        // TODO maybe separate / run tests per reduction on file
+//        try {
+//            for (Path classFile : context.classFiles) {
+//                reduce(classFile);
+//            }
+//        } catch (Exception e) {
+//            fatal("Could not complete reduction: {}", e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//        debug("Finished reduction", this, reducer);
+//
+//        // TODO use return values
+//        if (test()) {
+//            info("Test successful");
+//            try {
+//                if (dir.lift()) {
+//                    dir.clear();
+//                } else {
+//                    fatal("Could not copy result files to output directory");
+//                }
+//            } catch (IOException e) {
+//                fatal("Could not copy result files to output directory: {}", e.getMessage());
+//            }
+//        } else {
+//            info("Test failed");
+//            dir.clear();
+//        }
+//
+//        debug("Test directory cleared");
+//        debug("{}: Cleared test directory", this);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return name;
+//    }
+//
+//    private CtClass loadClass(Path path) throws IOException {
+//        try (InputStream is = Files.newInputStream(path)) {
+//            CtClass clazz = cp.makeClass(is);
+//            print(clazz);
+//            return clazz;
+//        }
+//    }
+//
+//    private void writeClass(CtClass clazz) throws IOException, CannotCompileException, NotFoundException {
+//        print(clazz);
+//        clazz.writeFile();
+//    }
+//
+//    private void reduce(Path path) throws Exception {
+//        CtClass clazz = loadClass(path);
+////        clazz = reducer.apply();
+//        writeClass(clazz);
+//    }
+//
+//    private boolean test() {
+//        return context.iTests.stream().map(i -> {
+//            try {
+//                return runner.exec(i);
+//            } catch (IOException e) {
+//                fatal("Could not complete test, as the script reported errors: {}", e.getMessage());
+//                return null;
+//            }
+//        }).allMatch(p -> {
+//            try {
+//                return p != null && p.waitFor() == ScriptRunner.EXIT_SUCCESS;
+//            } catch (InterruptedException e) {
+//                fatal("Could not complete test, as the process was interrupted: {}", e.getMessage());
+//                return false;
+//            }
+//        });
+//
+//    }
+//
+//    private void print(CtClass clazz) {
+//        PrintWriter writer = IoBuilder.forLogger()
+//                .setLevel(Level.DEBUG)
+//                .buildPrintWriter();
+//        ClassFilePrinter.print(clazz.getClassFile(), writer);
+//    }
+//
+//    private void debug(String message, Object... params) {
+//        logger.debug(this + ": " + message, params);
+//    }
+//
+//    private void info(String message, Object... params) {
+//        logger.info(this + ": " + message, params);
+//    }
+//
+//    private void fatal(String message, Object... params) {
+//        logger.fatal(this + ": " + message, params);
+//    }
 }
--- src/main/java/at/jku/ssw/java/bytecode/reducer/io/TestDirectory.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/io/TestDirectory.java
@@ -1,17 +1,5 @@
 package at.jku.ssw.java.bytecode.reducer.io;
 
-import at.jku.ssw.java.bytecode.reducer.context.Context;
-import at.jku.ssw.java.bytecode.reducer.utils.FileUtils;
-import at.jku.ssw.java.bytecode.reducer.utils.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.List;
-
 /**
  * This class takes care of the file handling for a single test instance.
  * This includes the initiation of the test files - copying the class files,
@@ -37,104 +25,104 @@ import java.util.List;
  * </ul>
  */
 public class TestDirectory {
-    //-------------------------------------------------------------------------
-    // region Constants
-
-    private static final Logger logger = LogManager.getLogger();
-
-    // endregion
-    //-------------------------------------------------------------------------
-    // region Properties
-
-    /**
-     * The current test context. Holds properties like the different paths.
-     */
-    private final Context context;
-
-    /**
-     * This directory's path.
-     */
-    private Path path;
-
-    /**
-     * Flag that indicates that the directory has already been cleared.
-     */
-    private boolean purged;
-
-    // endregion
-    //-------------------------------------------------------------------------
-    // region Constructor and overridden methods
-
-    public TestDirectory(Context context, String name) throws IOException {
-        assert context != null : "Cannot initialize test directory without context";
-        assert StringUtils.isNotBlank(name) : "Cannot initialize nameless test directory";
-
-        this.context = context;
-
-        this.purged = false;
-
-        // Set the current test directory
-        this.path = Files.createDirectories(context.tempDir.resolve(name));
-
-        // Copy the source files
-        copy(context.classFiles, this.path);
-        copy(context.iTests, this.path);
-    }
-
-    @Override
-    public String toString() {
-        return path.toString();
-    }
-
-// endregion
-    //-------------------------------------------------------------------------
-    // region Directory management methods
-
-    public boolean lift() throws IOException {
-        if (purged) return false;
-
-        final Path out = context.outDir;
-
-        // Copy the files into the output directory
-        Files.list(path)
-                .forEach(p -> {
-                    try {
-                        Files.copy(p, out.resolve(p.getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                    } catch (IOException e) {
-                        logger.fatal("Could not copy result file {} to output directory {}. The current results lie in {}: {}", p, out, path, e.getMessage());
-                    }
-                });
-
-        return true;
-    }
-
-    public boolean clear() {
-        if (purged) return true;
-
-        try {
-            FileUtils.delete(path);
-            return purged = true;
-        } catch (IOException e) {
-            logger.fatal("Could not clear test directory {}: {}", path, e.getMessage());
-        }
-
-        return false;
-    }
-
-    // endregion
-    //-------------------------------------------------------------------------
-    // region IO utilities
-
-    private void copy(List<Path> src, Path dst) {
-        src.forEach(p -> {
-            try {
-                Files.copy(p, dst.resolve(p.getFileName()), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                logger.fatal("Could not copy file {} to test directory {}: {}", p, dst, e.getMessage());
-            }
-        });
-    }
-
-    // endregion
-    //-------------------------------------------------------------------------
+//    //-------------------------------------------------------------------------
+//    // region Constants
+//
+//    private static final Logger logger = LogManager.getLogger();
+//
+//    // endregion
+//    //-------------------------------------------------------------------------
+//    // region Properties
+//
+//    /**
+//     * The current test context. Holds properties like the different paths.
+//     */
+//    private final Context context;
+//
+//    /**
+//     * This directory's path.
+//     */
+//    private Path path;
+//
+//    /**
+//     * Flag that indicates that the directory has already been cleared.
+//     */
+//    private boolean purged;
+//
+//    // endregion
+//    //-------------------------------------------------------------------------
+//    // region Constructor and overridden methods
+//
+//    public TestDirectory(Context context, String name) throws IOException {
+//        assert context != null : "Cannot initialize test directory without context";
+//        assert StringUtils.isNotBlank(name) : "Cannot initialize nameless test directory";
+//
+//        this.context = context;
+//
+//        this.purged = false;
+//
+//        // Set the current test directory
+//        this.path = Files.createDirectories(context.tempDir.resolve(name));
+//
+//        // Copy the source files
+//        copy(context.classFiles, this.path);
+//        copy(context.iTests, this.path);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return path.toString();
+//    }
+//
+//// endregion
+//    //-------------------------------------------------------------------------
+//    // region Directory management methods
+//
+//    public boolean lift() throws IOException {
+//        if (purged) return false;
+//
+//        final Path out = context.outDir;
+//
+//        // Copy the files into the output directory
+//        Files.list(path)
+//                .forEach(p -> {
+//                    try {
+//                        Files.copy(p, out.resolve(p.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+//                    } catch (IOException e) {
+//                        logger.fatal("Could not copy result file {} to output directory {}. The current results lie in {}: {}", p, out, path, e.getMessage());
+//                    }
+//                });
+//
+//        return true;
+//    }
+//
+//    public boolean clear() {
+//        if (purged) return true;
+//
+//        try {
+//            FileUtils.delete(path);
+//            return purged = true;
+//        } catch (IOException e) {
+//            logger.fatal("Could not clear test directory {}: {}", path, e.getMessage());
+//        }
+//
+//        return false;
+//    }
+//
+//    // endregion
+//    //-------------------------------------------------------------------------
+//    // region IO utilities
+//
+//    private void copy(List<Path> src, Path dst) {
+//        src.forEach(p -> {
+//            try {
+//                Files.copy(p, dst.resolve(p.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+//            } catch (IOException e) {
+//                logger.fatal("Could not copy file {} to test directory {}: {}", p, dst, e.getMessage());
+//            }
+//        });
+//    }
+//
+//    // endregion
+//    //-------------------------------------------------------------------------
 }
