8d2e2479fb1ce94584c88ad82783fea952e98525
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-22 09:20:06 +0200

    Add documentation for `MemberReducer`

--- src/main/java/at/jku/ssw/java/bytecode/reducer/runtypes/MemberReducer.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/runtypes/MemberReducer.java
@@ -8,9 +8,19 @@ import at.jku.ssw.java.bytecode.reducer.utils.functional.TFunction;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * Interface that provides methods to reduce or apply reduction operations
+ * on class members, where the concrete type of member can be specified
+ * by the implementing class.
+ *
+ * @param <CLASS>  The type for types
+ * @param <MEMBER> The type for members
+ *                 (e.g. implementations for fields, methods)
+ */
 public interface MemberReducer<CLASS, MEMBER>
         extends RepeatableReducer<MEMBER> {
 
+    @Override
     default Result<MEMBER> apply(Base<MEMBER> base) throws Exception {
 
         CLASS clazz = classFrom(base.bytecode());
@@ -26,6 +36,7 @@ public interface MemberReducer<CLASS, MEMBER>
                 .orElse(base.toMinimalResult());
     }
 
+    @Override
     default Result<MEMBER> force(byte[] bytecode) throws Exception {
         CLASS clazz = classFrom(bytecode);
 
@@ -39,11 +50,41 @@ public interface MemberReducer<CLASS, MEMBER>
         return base.toMinimalResult();
     }
 
+    /**
+     * Loads the given bytecode as an instance of the class type.
+     *
+     * @param bytecode The bytecode that describes the class to reduce
+     * @return a new instance of the class type representing the bytecode
+     * @throws Exception if the bytecode is invalid or the object
+     *                   cannot be instantiated
+     */
     CLASS classFrom(byte[] bytecode) throws Exception;
 
+    /**
+     * Retrieves the bytecode from the given instance of the class type.
+     *
+     * @param clazz The instance of the class type
+     * @return the bytecode describing the class
+     * @throws Exception if the bytecode cannot be extracted
+     */
     byte[] bytecodeFrom(CLASS clazz) throws Exception;
 
+    /**
+     * Retrieves potentially applicable members that should be attempted.
+     *
+     * @param clazz The class type instance
+     * @return a stream of potential members (of the given member type)
+     * @throws Exception if the members cannot be extracted / identified
+     */
     Stream<MEMBER> getMembers(CLASS clazz) throws Exception;
 
-    CLASS process(CLASS clazz, MEMBER MEMBER) throws Exception;
+    /**
+     * Processes the given member of the class.
+     *
+     * @param clazz  The class that this member belongs to
+     * @param member The member instance that should be processed
+     * @return the processed class instance
+     * @throws Exception if the processing failed
+     */
+    CLASS process(CLASS clazz, MEMBER member) throws Exception;
 }
