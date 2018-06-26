4da651d776355f09314ded4e45f877f5b89150e5
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-26 22:28:53 +0200

    [ShrinkConstantPool] Add implementation

---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/misc/ShrinkConstantPool.java
@@ -0,0 +1,20 @@
+package at.jku.ssw.java.bytecode.reducer.modules.misc;
+
+import at.jku.ssw.java.bytecode.reducer.annot.Sound;
+import at.jku.ssw.java.bytecode.reducer.runtypes.JavassistHelper;
+import at.jku.ssw.java.bytecode.reducer.runtypes.Reducer;
+
+/**
+ * Removes unused items from the constant pool.
+ */
+@Sound
+public class ShrinkConstantPool implements Reducer, JavassistHelper {
+    @Override
+    public byte[] apply(byte[] bytecode) throws Exception {
+        var clazz = classFrom(bytecode);
+
+        clazz.getClassFile().compact();
+
+        return bytecodeFrom(clazz);
+    }
+}
