785d16d7ed52f7a5e27f911d82b208a26af90641
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-27 21:15:15 +0200

    [ReplaceMethodCalls] Replace cached instance with line number comparison

--- src/main/java/at/jku/ssw/java/bytecode/reducer/modules/methods/ReplaceMethodCalls.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/methods/ReplaceMethodCalls.java
@@ -1,16 +1,16 @@
 package at.jku.ssw.java.bytecode.reducer.modules.methods;
 
-import at.jku.ssw.java.bytecode.reducer.annot.Sound;
+import at.jku.ssw.java.bytecode.reducer.annot.Unsound;
 import at.jku.ssw.java.bytecode.reducer.context.Reduction.Base;
 import at.jku.ssw.java.bytecode.reducer.context.Reduction.Result;
-import at.jku.ssw.java.bytecode.reducer.runtypes.RepeatableReducer;
 import at.jku.ssw.java.bytecode.reducer.runtypes.AssignmentReplacer;
-import at.jku.ssw.java.bytecode.reducer.utils.javassist.Instrumentation;
-import at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist;
+import at.jku.ssw.java.bytecode.reducer.runtypes.RepeatableReducer;
 import at.jku.ssw.java.bytecode.reducer.utils.functional.TConsumer;
 import at.jku.ssw.java.bytecode.reducer.utils.functional.TFunction;
 import at.jku.ssw.java.bytecode.reducer.utils.functional.TPredicate;
 import at.jku.ssw.java.bytecode.reducer.utils.javassist.Expressions;
+import at.jku.ssw.java.bytecode.reducer.utils.javassist.Instrumentation;
+import at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist;
 import javassist.CtClass;
 import javassist.expr.MethodCall;
 import org.apache.logging.log4j.LogManager;
@@ -19,14 +19,18 @@ import org.apache.logging.log4j.Logger;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
-@Sound
+/**
+ * Replaces method calls with default values of the corresponding return types.
+ * This reducer ignores {@code void} methods.
+ */
+@Unsound
 public class ReplaceMethodCalls
-        implements RepeatableReducer<MethodCall>, AssignmentReplacer {
+        implements RepeatableReducer<Integer>, AssignmentReplacer {
 
     private static final Logger logger = LogManager.getLogger();
 
     @Override
-    public Result<MethodCall> apply(Base<MethodCall> base) throws Exception {
+    public Result<Integer> apply(Base<Integer> base) throws Exception {
         final CtClass clazz = Javassist.loadClass(base.bytecode());
 
         final AtomicReference<MethodCall> call = new AtomicReference<>();
@@ -35,19 +39,27 @@ public class ReplaceMethodCalls
                 clazz,
                 (TPredicate<MethodCall>) c ->
                         !c.getMethod().getReturnType().equals(CtClass.voidType) &&
-                                !base.cache().contains(c) &&
+                                !base.cache().contains(c.getLineNumber()) &&
                                 call.compareAndSet(null, c),
                 (TConsumer<MethodCall>) c -> {
-                    CtClass type = c.getMethod().getReturnType();
+                    CtClass type  = c.getMethod().getReturnType();
+                    var     value = Expressions.defaults(type);
 
-                    c.replace(replaceWith(Expressions.defaults(type)));
+                    logger.debug(
+                            "Replacing call of method '{}' in line {} with '{}'",
+                            c.getMethodName(),
+                            c.getLineNumber(),
+                            value
+                    );
+
+                    c.replace(replaceWith(value));
                 }
         );
 
         // if no applicable member was found, the reduction is minimal
         return Optional.ofNullable(call.get())
-                .map((TFunction<MethodCall, Result<MethodCall>>) f ->
-                        base.toResult(Javassist.bytecode(clazz), f))
+                .map((TFunction<MethodCall, Result<Integer>>) f ->
+                        base.toResult(Javassist.bytecode(clazz), f.getLineNumber()))
                 .orElseGet(base::toMinimalResult);
     }
 }
