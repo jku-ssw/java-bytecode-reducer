10d982dab9e0387dca476aceb38eaf9b9f78ba3a
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-06-28 02:49:03 +0200

    Add abstract implementation for instruction based reducers

--- src/main/java/at/jku/ssw/java/bytecode/reducer/modules/flow/RemoveInstructionSequences.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/flow/RemoveInstructionSequences.java
@@ -2,27 +2,22 @@ package at.jku.ssw.java.bytecode.reducer.modules.flow;
 
 import at.jku.ssw.java.bytecode.reducer.annot.Expensive;
 import at.jku.ssw.java.bytecode.reducer.annot.Unsound;
-import at.jku.ssw.java.bytecode.reducer.context.Reduction.Base;
-import at.jku.ssw.java.bytecode.reducer.context.Reduction.Result;
-import at.jku.ssw.java.bytecode.reducer.runtypes.RepeatableReducer;
+import at.jku.ssw.java.bytecode.reducer.runtypes.InstructionReducer;
 import at.jku.ssw.java.bytecode.reducer.utils.CodePosition;
-import at.jku.ssw.java.bytecode.reducer.utils.functional.TFunction;
 import at.jku.ssw.java.bytecode.reducer.utils.javassist.Code;
-import at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist;
 import javassist.CtBehavior;
 import javassist.CtClass;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeIterator;
 import javassist.bytecode.Mnemonic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist.bytecode;
 import static javassist.bytecode.Opcode.NOP;
 
 /**
@@ -31,151 +26,96 @@ import static javassist.bytecode.Opcode.NOP;
  */
 @Expensive
 @Unsound
-public class RemoveInstructionSequences implements RepeatableReducer<CodePosition> {
+public class RemoveInstructionSequences implements InstructionReducer {
 
     private static final Logger logger = LogManager.getLogger();
 
-    /**
-     * Reduces the given class at the given code position by
-     * replacing all instructions withing the given range by NOPs.
-     *
-     * @param base         The reduction base
-     * @param clazz        The class to reduce
-     * @param codePosition The determined code position
-     * @return the reduction result
-     */
-    private Result<CodePosition> process(Base<CodePosition> base,
-                                         CtClass clazz,
-                                         CodePosition codePosition) {
+    @Override
+    public CtClass reduce(CtClass clazz,
+                          CtBehavior behav,
+                          CodePosition codePosition,
+                          CodeIterator it) {
 
-        /*
-        Fetch the method that is referenced in the code position.
-        As this always returns a single method, the Optional result is simply
-        forced.
-        */
-        return Arrays.stream(clazz.getDeclaredBehaviors())
-                .filter(m -> m.getLongName().equals(codePosition.member))
-                .findAny()
-                .map(method -> {
+        var begin = codePosition.begin;
+        var end   = codePosition.end;
 
-                    var methodInfo = method.getMethodInfo();
+        logger.debug(
+                "Removing instructions of behaviour '{}' from index {} to {}",
+                behav.getLongName(),
+                begin,
+                end
+        );
 
-                    var ca = methodInfo.getCodeAttribute();
-                    var it = ca.iterator();
+        // replace the determined instruction range with NOPs
+        IntStream.range(begin, end)
+                .forEach(i -> it.writeByte(NOP, i));
 
-                    var begin = codePosition.begin;
-                    var end   = codePosition.end;
-
-                    logger.debug(
-                            "Removing instructions of behaviour '{}' from index {} to {}",
-                            method.getLongName(),
-                            begin,
-                            end
-                    );
-
-                    // replace the determined instruction range with NOPs
-                    IntStream.range(begin, end)
-                            .forEach(i -> it.writeByte(NOP, i));
-
-                    try {
-                        return base.toResult(bytecode(clazz), codePosition);
-                    } catch (IOException e) {
-                        return null;
-                    }
-                }).orElse(null);
+        return clazz;
     }
 
     @Override
-    public Result<CodePosition> apply(Base<CodePosition> base) throws Exception {
-        final var clazz = Javassist.loadClass(base.bytecode());
+    public Stream<CodePosition> codePositions(CtClass clazz, CtBehavior method, CodeIterator it) throws BadBytecode {
+        var name = method.getLongName();
 
-        // iterate all "behaviours" (which includes methods and initializers)
-        return Arrays.stream(clazz.getDeclaredBehaviors())
-                .flatMap((TFunction<CtBehavior, Stream<CodePosition>>) method -> {
-                    var m = method.getMethodInfo();
+        logger.trace(name);
 
-                    // the key that uniquely identifies this method
-                    var name = method.getLongName();
+        // store the markings at which the stack size is zero
+        // and that are not NOPs
+        var beginIndices = new ArrayList<Integer>();
 
-                    logger.trace(name);
+        // store the markings at which the stack size is zero
+        // which may be NOPs
+        var endIndices = new ArrayList<Integer>();
 
-                    final var ca = m.getCodeAttribute();
-                    final var it = ca.iterator();
+        // the current number of items on the stack
+        var stackSize = 0;
 
-                    // store the markings at which the stack size is zero
-                    // and that are not NOPs
-                    var beginIndices = new ArrayList<Integer>();
+        while (it.hasNext()) {
+            int index = it.next();
 
-                    // store the markings at which the stack size is zero
-                    // which may be NOPs
-                    var endIndices = new ArrayList<Integer>();
+            // get the opcode at the current index position
+            int code = it.byteAt(index);
 
-                    // the current number of items on the stack
-                    var stackSize = 0;
+            // If the stacksize is zero BEFORE the current
+            // instruction, either a previous sequence was
+            // discarded or the loop just started
+            if (stackSize == 0 && code != NOP)
+                beginIndices.add(index);
 
-                    /*
-                        Every constructor code begins with
-                        a call to the initialization method:
-                        aload_0
-                        invokespecial #1
+            var oldStackSize = stackSize;
 
-                        In case of a constructor method, those first
-                        two instructions are therefore skipped,
-                        as the stack is again empty after this sequence.
-                    */
-                    it.skipConstructor();
+            // calculate the new stack level
+            stackSize = Code.newStackLevel(
+                    method,
+                    oldStackSize,
+                    code,
+                    index,
+                    it
+            );
 
-                    while (it.hasNext()) {
-                        int index = it.next();
+            logger.trace(String.format(
+                    "%6d: %-40s // Stack: %d -> %d",
+                    index,
+                    Mnemonic.OPCODE[code],
+                    oldStackSize,
+                    stackSize
+            ));
 
-                        // get the opcode at the current index position
-                        int code = it.byteAt(index);
+            // if the stack is empty (again), remember the next index
+            // as this may be a potential end of a removable sequence
+            if (stackSize == 0)
+                endIndices.add(it.lookAhead());
+        }
 
-                        // If the stacksize is zero BEFORE the current
-                        // instruction, either a previous sequence was
-                        // discarded or the loop just started
-                        if (stackSize == 0 && code != NOP)
-                            beginIndices.add(index);
+        // perform sorting before return expression
+        endIndices.sort(Comparator.reverseOrder());
 
-                        var oldStackSize = stackSize;
-
-                        // calculate the new stack level
-                        stackSize = Code.newStackLevel(
-                                method,
-                                oldStackSize,
-                                code,
-                                index,
-                                it
-                        );
-
-                        logger.trace(String.format(
-                                "%6d: %-40s // Stack: %d -> %d",
-                                index,
-                                Mnemonic.OPCODE[code],
-                                oldStackSize,
-                                stackSize
-                        ));
-
-                        // if the stack is empty (again), remember the next index
-                        // as this may be a potential end of a removable sequence
-                        if (stackSize == 0)
-                            endIndices.add(it.lookAhead());
-                    }
-
-                    // perform sorting before return expression
-                    endIndices.sort(Comparator.reverseOrder());
-
-                    return beginIndices.stream()
-                            .flatMap(i ->
-                                    endIndices.stream()
-                                            .filter(j -> j > i)
-                                            .map(j -> new CodePosition(name, i, j))
-                            )
-                            .filter(cp -> !base.cache().contains(cp));
-                })
-                .findAny()
-                .map(cp -> process(base, clazz, cp))
-                .orElseGet(base::toMinimalResult);
+        return beginIndices.stream()
+                .flatMap(i ->
+                        endIndices.stream()
+                                .filter(j -> j > i)
+                                .map(j -> new CodePosition(name, i, j))
+                );
     }
 
 }
---
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/runtypes/InstructionReducer.java
@@ -0,0 +1,124 @@
+package at.jku.ssw.java.bytecode.reducer.runtypes;
+
+import at.jku.ssw.java.bytecode.reducer.context.Reduction.Base;
+import at.jku.ssw.java.bytecode.reducer.context.Reduction.Result;
+import at.jku.ssw.java.bytecode.reducer.utils.CodePosition;
+import at.jku.ssw.java.bytecode.reducer.utils.functional.TFunction;
+import at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeIterator;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static at.jku.ssw.java.bytecode.reducer.utils.javassist.Javassist.bytecode;
+
+/**
+ * Run type that allows low level access to method behaviour.
+ * Caches code positions that were already tried.
+ */
+public interface InstructionReducer extends RepeatableReducer<CodePosition> {
+
+    /**
+     * Perform the reduction operation on the given code position
+     * using the given iterator.
+     *
+     * @param clazz        The currently reduced class
+     * @param behav        The current method
+     * @param codePosition The code position that should be reduced
+     * @param iterator     The code iterator that allows low level access
+     * @return the reduced class
+     */
+    CtClass reduce(CtClass clazz,
+                   CtBehavior behav,
+                   CodePosition codePosition,
+                   CodeIterator iterator);
+
+    /**
+     * Find applicable code positions in the given behaviour with the
+     * given code iterator.
+     *
+     * @param clazz  The currently reduced class
+     * @param method The current method
+     * @param it     The code iterator that allows low level access
+     * @return all applicable code positions
+     * @throws BadBytecode if the byte code is invalid at some point
+     */
+    Stream<CodePosition> codePositions(CtClass clazz,
+                                       CtBehavior method,
+                                       CodeIterator it) throws BadBytecode;
+
+    /**
+     * Reduces the given class at the given code position by the
+     * implementation dependent operation.
+     *
+     * @param base         The reduction base
+     * @param clazz        The class to reduce
+     * @param codePosition The determined code position
+     * @return the reduction result
+     */
+    default Result<CodePosition> process(Base<CodePosition> base,
+                                         CtClass clazz,
+                                         CodePosition codePosition) {
+
+        /*
+        Fetch the method that is referenced in the code position.
+        As this always returns a single method, the Optional result is simply
+        forced.
+        */
+        return Arrays.stream(clazz.getDeclaredBehaviors())
+                .filter(b -> b.getLongName().equals(codePosition.member))
+                .findAny()
+                .map(behav -> {
+
+                    var methodInfo = behav.getMethodInfo();
+
+                    var ca = methodInfo.getCodeAttribute();
+                    var it = ca.iterator();
+
+                    try {
+                        return base.toResult(bytecode(
+                                reduce(clazz, behav, codePosition, it)
+                        ), codePosition);
+                    } catch (IOException e) {
+                        return null;
+                    }
+                }).orElse(null);
+    }
+
+    @Override
+    default Result<CodePosition> apply(Base<CodePosition> base) throws Exception {
+        final var clazz = Javassist.loadClass(base.bytecode());
+
+        // iterate all "behaviours" (which includes methods and initializers)
+        return Arrays.stream(clazz.getDeclaredBehaviors())
+                .flatMap((TFunction<CtBehavior, Stream<CodePosition>>) method -> {
+                    var m = method.getMethodInfo();
+
+                    final var ca = m.getCodeAttribute();
+                    final var it = ca.iterator();
+
+                    /*
+                        Every constructor code begins with
+                        a call to the initialization method:
+                        aload_0
+                        invokespecial #1
+
+                        In case of a constructor method, those first
+                        two instructions are therefore skipped,
+                        as the stack is again empty after this sequence.
+                    */
+                    it.skipConstructor();
+
+                    return codePositions(clazz, method, it)
+                            .filter(cp -> !base.cache().contains(cp));
+                })
+                .findAny()
+                .map(cp -> process(base, clazz, cp))
+                .orElseGet(base::toMinimalResult);
+    }
+
+}
