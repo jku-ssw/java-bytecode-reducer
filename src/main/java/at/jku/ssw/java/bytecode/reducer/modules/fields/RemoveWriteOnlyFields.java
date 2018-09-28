d722b7591ff4d0788b32cae4200cad470a6bfcb6
Sebastian Kloibhofer <sebastian.kloibhofer@gmail.com>
2018-09-28 18:32:59 +0200

    Fix rare case that `RemoveWriteOnlyFields` runs into an infinite loop

--- src/main/java/at/jku/ssw/java/bytecode/reducer/modules/fields/RemoveWriteOnlyFields.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/modules/fields/RemoveWriteOnlyFields.java
@@ -2,7 +2,7 @@ package at.jku.ssw.java.bytecode.reducer.modules.fields;
 
 import at.jku.ssw.java.bytecode.reducer.annot.Sound;
 import at.jku.ssw.java.bytecode.reducer.runtypes.JavassistHelper;
-import at.jku.ssw.java.bytecode.reducer.runtypes.InstanceCachedMemberReducer;
+import at.jku.ssw.java.bytecode.reducer.runtypes.MemberReducer;
 import at.jku.ssw.java.bytecode.reducer.utils.javassist.Instrumentation;
 import javassist.CtClass;
 import javassist.CtField;
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 @Sound
 public class RemoveWriteOnlyFields
-        implements InstanceCachedMemberReducer<CtClass, CtField>, JavassistHelper {
+        implements MemberReducer<CtClass, CtField, String>, JavassistHelper {
 
     private static final Logger logger = LogManager.getLogger();
 
@@ -28,5 +28,10 @@ public class RemoveWriteOnlyFields
         logger.debug("Removing field '{}'", field.getName());
         clazz.removeField(field);
         return clazz;
+    }
+
+    @Override
+    public String keyFromMember(CtField ctField) {
+        return ctField.getName();
     }
 }
