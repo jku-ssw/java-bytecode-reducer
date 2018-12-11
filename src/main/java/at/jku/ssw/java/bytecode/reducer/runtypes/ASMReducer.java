8c8b9f4446a7b2a32cdb83c915a5bc9ecfe46a08
skloibi <sebastian.kloibhofer@gmail.com>
2018-12-11 08:54:43 +0100

    Remove ASM frame computation from `ASMReducer`

--- src/main/java/at/jku/ssw/java/bytecode/reducer/runtypes/ASMReducer.java
+++ src/main/java/at/jku/ssw/java/bytecode/reducer/runtypes/ASMReducer.java
@@ -6,14 +6,18 @@ import org.objectweb.asm.ClassWriter;
 
 import java.util.stream.Stream;
 
-public interface ASMReducer extends Reducer {
+public interface ASMReducer extends Reducer {//IterativeReducer<CodePosition> {
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default byte[] apply(byte[] bytecode) {
 
         // do not use "optimized" ClassWriter invocation (with reference
         // to the ClassReader instance, since this is discouraged for
         // non-additive transformations
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
         ClassReader cr = new ClassReader(bytecode);
 
         visitors(cw).forEach(v -> cr.accept(v, 0));
